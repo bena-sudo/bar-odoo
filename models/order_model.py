51ad50ce15b1cabc09acf3f2489cc17191c068c4
Joan43 <joanbenaventcastells@gmail.com>
2022-12-04 07:07:28 -0500

    Calcular el precio total

--- models/order_model.py
+++ models/order_model.py
@@ -10,9 +10,15 @@ class OrderModel(models.Model):
     table = fields.Many2one("bar_app.table_model", string="Table")
     creationdate = fields.Datetime(srting="Date",help="Date",requiered=True,default=lambda self: datetime.now())
     lines = fields.One2many("bar_app.line_model", "cuantity" , string="Products", requiered=True)
-    tprice = fields.Float(string="Total price",help="Total price of the order.")
+    tprice = fields.Float(string="Total price",compute="_getTotalPrice",store=True)
     
     @api.constrains("numclients")
     def _checkValue(self):
         if self.numclients <= 0:
             raise ValidationError("The value of the number client order must be less than 0.")
+
+    @api.depends("lines")
+    def _getTotalPrice(self):
+        self.tprice = 0
+        for line in self.lines:
+            self.tprice += line.cuantity * line.product.price
