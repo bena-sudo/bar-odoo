902d8f18de80b8cb9312e90e222c63644c6812f5
Joan43 <joanbenaventcastells@gmail.com>
2022-12-14 10:39:58 -0500

    Api para ingredients

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -60,11 +60,11 @@ class BarApp(http.Controller):
 
     # DELETE CATEGORY
     @http.route('/bar_app/deleteCategory', auth="public", type="json", method="DELETE")
-    def deleteCat(self, **kw):
+    def deleteCategory(self, **kw):
         response = request.jsonrequest
         domain = [("id", "=", response["id"])]
         try:
-            x = http.request.env["task_app.category_model"].sudo().search(
+            x = http.request.env["bar_app.category_model"].sudo().search(
                 domain).unlink()
             data = {"status": 200, "result": "Category deleted"}
             return data
@@ -81,6 +81,64 @@ class BarApp(http.Controller):
         data = {"status": 200,
                 "data": tdata}
         return http.Response(json.dumps(data).encode("utf8"), mimetype="application/json")
+
+	# ADD INGREDIENT
+    @http.route('/bar_app/addIngredient', auth='public', type="json", method="POST")
+    def addIngredient(self, **kw):
+        response = request.jsonrequest
+        try:
+            result = http.request.env["bar_app.ingredient_model"].sudo().create(
+                response)
+            data = {"status": 201, "id": result.id}
+            return data
+        except Exception as e:
+            data = {"status": 404, "error": e}
+        return data
+
+	# GET INGREDIENT BY ID
+    @http.route('/bar_app/getIngredient/<int:id>', auth='public', type="http")
+    def getIngredientById(self, id=None, **kw):
+        if id:
+            domain = [{"id", "=", id}]
+        else:
+            domain = []
+        taskdata = http.request.env["bar_app.ingredient_model"].sudo().search_read(
+            domain, ["name", "products", "description"])
+        data = {"status": 200,
+                "data": taskdata}
+        return http.Response(json.dumps(data).encode("utf8"), mimetype="application/json")
+
+	# UPDATE INGREDIENT
+    @http.route('/bar_app/updateIngredient', auth="public", type="json", method="PUT")
+    def updateIngredient(self, **kw):
+        response = request.jsonrequest
+        domain = [("id", "=", response["id"])]
+        try:
+            x = http.request.env["bar_app.ingredient_model"].sudo().search(
+                domain)
+            updated = x.sudo().write(response)
+            if (updated):
+                data = {"status": 200, "result": x.id}
+            else:
+                data = {"status": 400, "result": "Ingredient not modified"}
+            return data
+        except Exception as e:
+            data = {"status": 404, "error": e}
+            return data
+
+	# DELETE INGREDIENT
+    @http.route('/bar_app/deleteIngredient', auth="public", type="json", method="DELETE")
+    def deleteIngredient(self, **kw):
+        response = request.jsonrequest
+        domain = [("id", "=", response["id"])]
+        try:
+            x = http.request.env["bar_app.ingredient_model"].sudo().search(
+                domain).unlink()
+            data = {"status": 200, "result": "Ingredient deleted"}
+            return data
+        except Exception as e:
+            data = {"status": 404, "error": e}
+            return data
 
 # PRODUCT
     # GET PRODUCTS
